70d4dc20512428d15c634ed9b9f0f2ba5beb9a17
Robert Evanik <rob.evanik@gmail.com>
2021-11-26 16:13:51 -0500

    styled the input box

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
-console.log("test")
+var searchBoxEl = document.querySelector("#search")
+var cityInputEl = document.querySelector(".validate")
+
+
+
+var submitBtnHandler = function(event) {
+    event.preventDefault();
+
+    var city = cityInputEl.value.trim();
+    console.log(city);
+}
+
+
+
+
+
+
+
+searchBoxEl.addEventListener("click", submitBtnHandler)
