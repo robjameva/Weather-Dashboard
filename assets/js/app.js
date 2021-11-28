da89bf729123b163ce426a54420f41a7e57b512c
Robert Evanik <rob.evanik@gmail.com>
2021-11-27 21:02:54 -0500

    Prevented dupes

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ var uvWrapperEl = document.querySelector("#uv-background")
 var lat;
 var lon;
 
-
+var defaultLocations = ["New Jersey", "California", "Whippany"];
 var today = moment().format("MMM Do");
 var myAPIKey = 'b58b9f0f57c16a04f0ddb33fe7147ac6'
 var recentSearch = [];
@@ -29,7 +29,6 @@ var setDates = function() {
     }
 }
 
-
 var submitBtnHandler = function(event) {
     event.preventDefault();
 
@@ -39,11 +38,10 @@ var submitBtnHandler = function(event) {
         cityInputEl.value = "";
     }
     else {
-        alert("Please Enter a City Name")
+        alert("Please Enter a City Name");
     }
 
 }
-
 
 var getWeather = function(cityName) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${myAPIKey}&units=imperial`
@@ -52,23 +50,22 @@ var getWeather = function(cityName) {
             if (response.ok) {
                 response.json().then(function(data) {
                     populateToday(data);
-                    setHeadingIcon(data)
+                    setHeadingIcon(data);
                     setUVIndex();
-                    console.log(data)
                     populateFiveDay(data);
-                    generateRecentSearchBtn(cityName);
-                    localStorage.setItem("cities", JSON.stringify(recentSearch))
+                    generateRecentSearchBtn(data.city.name);
+                    localStorage.setItem("cities", JSON.stringify(recentSearch));
                     currentCity = cityName;
-                    displayCity();
                 });
             }
             else {
-                alert("City Not Found")
+                alert("City Not Found");
             }
         })
 }
 
 var populateToday = function(data) {
+    cityDisplayEl.textContent = data.city.name;
     tempEl.textContent = data.list[2].main.temp;
     windEl.textContent = data.list[2].wind.speed;
     humidEl.textContent = data.list[2].main.humidity;
@@ -78,7 +75,7 @@ var populateToday = function(data) {
 
 var setHeadingIcon = function(data) {
     var iconEl = document.querySelector(".heading-icon");
-    var iconUrl = "http://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + "@2x.png"
+    var iconUrl = "http://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + "@2x.png";
     iconEl.setAttribute("src", iconUrl);
 }
 
@@ -86,17 +83,14 @@ var populateFiveDay = function(data) {
     var nextDay = 0;
     for (var i = 0; i < eachDay.length; i++) {
         var child = eachDay[i];
-        var imgUrl = "http://openweathermap.org/img/wn/" + data.list[nextDay].weather[0].icon + "@2x.png"
-        child.children[1].setAttribute("src", imgUrl)
+        var imgUrl = "http://openweathermap.org/img/wn/" + data.list[nextDay].weather[0].icon + "@2x.png";
+        child.children[1].setAttribute("src", imgUrl);
         child.children[2].textContent = "Temp: " + data.list[nextDay].main.temp + " °F";
         child.children[3].textContent = "Wind: " + data.list[nextDay].wind.speed + " MPH";
         child.children[4].textContent = "Humidity: " + data.list[nextDay].main.humidity + " %";
         nextDay += 5;
-        console.log(nextDay)
     }
 }
-
-
 
 var setUVIndex = function() {
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${myAPIKey}`
@@ -106,16 +100,16 @@ var setUVIndex = function() {
                 response.json().then(function(data) {
                     uvEl.textContent = data.current.uvi;
                     if (data.current.uvi >= 8) {
-                        uvWrapperEl.style.setProperty('--uv-index', 'rgb(219, 13, 13)')
+                        uvWrapperEl.style.setProperty('--uv-index', 'rgb(219, 13, 13)');
                     }
                     else if (data.current.uvi >= 6) {
-                        uvWrapperEl.style.setProperty('--uv-index', 'rgb(245, 170, 31)')
+                        uvWrapperEl.style.setProperty('--uv-index', 'rgb(245, 170, 31)');
                     }
                     else if (data.current.uvi >= 3) {
-                        uvWrapperEl.style.setProperty('--uv-index', 'rgb(230, 230, 36)')
+                        uvWrapperEl.style.setProperty('--uv-index', 'rgb(230, 230, 36)');
                     }
                     else {
-                        uvWrapperEl.style.setProperty('--uv-index', 'rgb(155, 216, 155)')
+                        uvWrapperEl.style.setProperty('--uv-index', 'rgb(155, 216, 155)');
                     }
                 });
             }
@@ -124,21 +118,14 @@ var setUVIndex = function() {
 }
 
 var generateRecentSearchBtn = function(city) {
-    if (!recentSearch.includes(city) && recentSearch.length < 10) {
+    if (!recentSearch.includes(city)) {
         var cityBtnEl = document.createElement("a");
         cityBtnEl.classList = "waves-effect waves-light btn-large search-btn";
-        cityBtnEl.setAttribute("data-city", city)
+        cityBtnEl.setAttribute("data-city", city);
         cityBtnEl.textContent = city;
 
         btnContainerEl.appendChild(cityBtnEl);
         recentSearch.push(city);
-
-    }
-    else if (recentSearch.length === 10) {
-        recentSearch.shift()
-        recentSearch.push(city);
-        localStorage.setItem("cities", JSON.stringify(recentSearch))
-        loadRecentSearch();
     }
 }
 
@@ -146,7 +133,6 @@ var getWeatherFromBtn = function(event) {
     var city = event.target.getAttribute("data-city");
     getWeather(city);
 }
-
 
 var clearBtnHandler = function() {
     var loadedSearches = JSON.parse(localStorage.getItem("cities"))
@@ -158,53 +144,28 @@ var clearBtnHandler = function() {
     localStorage.setItem("cities", "");
 }
 
-
 var loadRecentSearch = function() {
-    var loadedSearches = JSON.parse(localStorage.getItem("cities"))
+    var loadedSearches = JSON.parse(localStorage.getItem("cities"));
 
     for (let i = 0; i < loadedSearches.length; i++) {
         generateRecentSearchBtn(loadedSearches[i]);
     }
 }
 
-var displayCity = function() {
-    var words = currentCity.split(' ');
-    var result = "";
-    for (var i = 0; i < words.length; i++) {
-        var word = words[i]
-        var firstLetter = word[0]
-        var otherLetters = word.slice(1);
-        var upper = firstLetter.toUpperCase();
-        result += upper + otherLetters + " ";
-    }
-    currentCity = result.trim();
-    cityDisplayEl.textContent = currentCity;
-}
-
 var setDefaults = function() {
-    getWeather("New Jersey");
-    getWeather("California");
-    getWeather("Whippany");
+    for (var i = 0; i < defaultLocations.length; i++) {
+        getWeather(defaultLocations[i]);
+    }
+    console.log(recentSearch)
 }
-
-
-
-
-
-
-
-
-
-
-
 
 if (localStorage.getItem("cities")) {
     loadRecentSearch();
 }
 
-searchBoxEl.addEventListener("click", submitBtnHandler)
-clearBtnEl.addEventListener("click", clearBtnHandler)
-btnContainerEl.addEventListener("click", getWeatherFromBtn)
-
 setDefaults();
 setDates();
+
+searchBoxEl.addEventListener("click", submitBtnHandler);
+clearBtnEl.addEventListener("click", clearBtnHandler);
+btnContainerEl.addEventListener("click", getWeatherFromBtn);
